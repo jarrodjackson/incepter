1c869d0db2455cea013ddd7732c46c9e5191514e
Jarrod Jackson <github@thespinlight.com>
2019-01-16 15:20:54 -0800

    Add installation scripts.

---
+++ scripts/install-terraform.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+readonly TERRAFORM_VERSION="0.11.11"
+readonly INSTALL_DIR="/usr/local/bin"
+readonly DOWNLOAD_DIR="/tmp"
+readonly DOWNLOAD_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+readonly DOWNLOADED_FILE="$DOWNLOADED_DIR/terraform.zip"
+
+echo ""
+echo "Downloading Terraform ${TERRAFORM_VERSION}..."
+curl -o "$DOWNLOADED_FILE" "$DOWNLOAD_URL"
+echo ""
+echo "Extracting Terraform to ${INSTALL_DIR}..."
+unzip "$DOWNLOADED_FILE" -d "$INSTALL_DIR"
+echo ""
+echo "Cleaning up..."
+rm "$DOWNLOADED_FILE"
